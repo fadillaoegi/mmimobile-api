389081409b857f17072fa044f9d53a3b97de1660
Fadil <fadilfdl2710@gmail.com>
2025-01-06 08:35:24 +0700

    save

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -15,30 +15,6 @@ class Api extends RestController
         $this->User_model_api = $this->User_model_api;
     }
 
-    public function testHash_get()
-    {
-        $passExample = "aku123123";
-        $passHash = password_hash($passExample, PASSWORD_DEFAULT);
-        $passInfo = password_get_info($passExample);
-        $passInfoHash = password_get_info($passHash);
-
-        // if ($passInfoHash['algo'] === 0) {
-        //     $this->set_response(['pass_not_hash' => $passExample,], RestController::HTTP_UNAUTHORIZED);
-        // } else {
-        //     $this->set_response(['pass_hash' => $passHash,], RestController::HTTP_OK);
-        // }
-
-        if (!password_verify($passExample, $passHash)) {
-            $this->set_response([
-                'message' => "password invalid !"
-            ], RestController::HTTP_UNAUTHORIZED);
-            return;
-        }
-        $this->set_response([
-            'message' => "password valid !"
-        ], RestController::HTTP_UNAUTHORIZED);
-    }
-
     // NOTE: SIGN IN API
     public function signIn_post()
     {
@@ -66,12 +42,16 @@ class Api extends RestController
 
             return;
         } else {
-            $user = $this->User_model_api->getUserByPhone($phone);
-            $pelanggan = false;
-            $customer_pass_default = false;
+            // NOTE: DUMMY
             // $passInfo = password_get_info($user['customer_password']);
             // $passHashh = password_hash($user['customer_password'], PASSWORD_DEFAULT);
             // $passInfo = password_get_info($passHashh);
+            // "password": "aku123123",
+            // "passwordHash": "$2y$10$7DkViUV0fLSYHQWDV.hQaulwjPJWxTxScmQNOJXdzOjSUv8iPEnJi"
+
+            $user = $this->User_model_api->getUserByPhone($phone);
+            $pelanggan = false;
+            $customer_pass_default = false;
 
             // NOTE: CHECKING USER
             if (!$user) {
@@ -105,9 +85,6 @@ class Api extends RestController
                 return;
             }
 
-            // 085339333168 => data customer_type_id 0
-            // 081333522596 => data customer_type_id 1
-
             // NOTE: CHECKING CUSTOMER TYPE
             if ($user['customer_type_id'] > 0) $pelanggan = true;
 
@@ -123,9 +100,9 @@ class Api extends RestController
                     'customer_date_birth' => $user['customer_date_birth'],
                     'customer_phone' => $user['customer_phone'],
                     'customer_type_id' => $user['customer_type_id'],
-                    'province_id' => $user['province_id'],
-                    'city_id' => $user['city_id'],
-                    'subdistrict_id' => $user['subdistrict_id'],
+                    'province_id' => $user['province_id'] == null ? "0" : $user['province_id'],
+                    'city_id' => $user['city_id']== null ? "0" : $user['city_id'],
+                    'subdistrict_id' => $user['subdistrict_id']== null ? "0" : $user['subdistrict_id'],
                 ]
             ], RestController::HTTP_OK);
         }
